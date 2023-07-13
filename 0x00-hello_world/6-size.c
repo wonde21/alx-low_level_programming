e82d43f2e7f1b8565e8e67e57ab70818ca10da60
wonde21 <wendim878@gmail.com>
2023-07-13 15:57:14 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	int sllint[1] = sizeof(long long int);
 	int sfloat[1] = sizeof(float);
 
-	printf("Size of a char: %i byte(s)\n", &schar);
-	printf("Size of an int: %i byte(s)\n", &sint);
-	printf("Size of a long int: %i byte(s)\n", &slint);
-	printf("Size of a long long int: %i byte(s)\n", &sllint);
-	printf("Size of a float: %i byte(s),\n", &sfloat);
+	printf("Size of a char: %d byte(s)\n", &schar);
+	printf("Size of an int: %d byte(s)\n", &sint);
+	printf("Size of a long int: %d byte(s)\n", &slint);
+	printf("Size of a long long int: %d byte(s)\n", &sllint);
+	printf("Size of a float: %d byte(s),\n", &sfloat);
 	return (0);
 }
