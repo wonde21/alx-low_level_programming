9a21be85caebe84907281cf529ec1d743cf2af07
wonde21 <wendim878@gmail.com>
2023-07-13 12:58:45 -0700

    4-puts.c added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+* main - Entry point
+* Description:'the program's description'
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
