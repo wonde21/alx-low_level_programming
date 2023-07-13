2e029e5f3051ba3184c42c5d33c9b97e2e942165
wonde21 <wendim878@gmail.com>
2023-07-13 14:12:27 -0700

    6-size.c fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,1 @@
-#include <stdio.h>
-/**
- * main - Entry point.
- * Descrioption:'the program's description'
- * Return: Always 0 (Seccess)
- */
-int main(void)
-{
-	printf("the size of an int %i", &sizeof(int i));
-	return (0);
-}
+	printf("the size of an int %i", sizeof(int));
