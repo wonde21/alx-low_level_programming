d0f59802528fb2ebe968913b6e1835a5fe1f8451
wonde21 <wendim878@gmail.com>
2023-07-13 13:52:36 -0700

    6-size.c added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Entry point.
+ * Descrioption: 'the program's description'
+ * Return: Always 0 (Seccess)
+ */
+int main (void)
+{
+        int i;
+        printf("Size of an int %i", sizeof(i);
+}
