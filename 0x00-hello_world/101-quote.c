cf4f4b25c93fb3952c8ceddb6d5534d8ffb30858
wonde21 <wendim878@gmail.com>
2023-07-13 17:10:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h
+/**
+ * main - Entry point
+ * Description: 'AlX 101-quote.c'
+ * Return 1 (Failer)
+ */
+int main (void)
+{
+	fputchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
