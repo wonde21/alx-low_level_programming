7ce6aecdae7fe3dc28edf429f92d91463f16dd56
wonde21 <wendim878@gmail.com>
2023-07-15 11:51:40 -0700

    102-print_comb5.c added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - Entry point
+ * description: 'print the combination of two two digit number'
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	bool first_cheaker = true;
+
+	for (int num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (int num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (int num3 = num1; num3 <= '9'; num3++)
+			{
+				for (int num4 = num2 + 1; num4 <= '9'; num4++)
+				{
+					if (first_cheaker)
+					{
+						first_cheaker = false;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(num1);
+					putchar(num2);
+					putchar(' ');
+					putchar(num3);
+					putchar(num4);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
