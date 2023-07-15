f48b4695398630556d8f12bfe8c60466ff4a867c
wonde21 <wendim878@gmail.com>
2023-07-15 11:54:08 -0700

    102-print_comb5.c fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,13 +15,13 @@ int main(void)
 	int num4;
 	bool first_cheaker = true;
 
-	for (int num1 = '0'; num1 <= '9'; num1++)
+	for ( num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (int num2 = '0'; num2 <= '9'; num2++)
+		for ( num2 = '0'; num2 <= '9'; num2++)
 		{
-			for (int num3 = num1; num3 <= '9'; num3++)
+			for ( num3 = num1; num3 <= '9'; num3++)
 			{
-				for (int num4 = num2 + 1; num4 <= '9'; num4++)
+				for ( num4 = num2 + 1; num4 <= '9'; num4++)
 				{
 					if (first_cheaker)
 					{
