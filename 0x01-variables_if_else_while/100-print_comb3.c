0f7f537f47d5da9f48e3278aa35ba59735f32ed4
wonde21 <wendim878@gmail.com>
2023-07-15 09:54:26 -0700

    100-print_comb3.c fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,20 +12,22 @@ int main(void)
 	int num1;
 	int num2;
 	bool first_cheaker = true;
-
-	for (num2 = num + '1'; num2 <= '9'; num2++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		if (first_cheaker)
+		for (num2 = num1 + '1'; num2 <= '9'; num2++)
 		{
-			first_cheaker = false;
+			if (first_cheaker)
+			{
+				first_cheaker = false;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(num1);
+			putchar(num2);
 		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar(num1);
-		putchar(num2);
 	}
 	putchar('\n');
 	return (0);
