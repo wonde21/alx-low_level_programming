60856dabeff8d5fcd4cde4941fc83ff24d2ef422
wonde21 <wendim878@gmail.com>
2023-07-18 08:16:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,8 +12,11 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		print_alphabet();
-		_putchar('\n');
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+			_putchar('\n');
+		}
 		i++;
 	}
 }
