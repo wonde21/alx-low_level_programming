054f9ee069cd67829934477f4b772978a50ad10c
wonde21 <wendim878@gmail.com>
2023-07-18 08:52:10 -0700

    3-islower.c fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include "main.h"
 
 /**
@@ -5,8 +6,8 @@
  * Return: Always 0
  */
 
-int _islower(int c){
-	if (c < 'c')
+int _islower(char c){
+	if (islower(c))
 	{
 		return (1);
 	}
