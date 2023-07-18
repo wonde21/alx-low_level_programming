d115f01311d8263141c4d154b0e119eb2095529f
wonde21 <wendim878@gmail.com>
2023-07-18 09:49:00 -0700

    5-sign.c adde

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - print the sign of num
+ * @n: identifyable num
+ * Return: 1 if + and 0 if 0 and -1 if -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return ("-1");
+	}
+}
