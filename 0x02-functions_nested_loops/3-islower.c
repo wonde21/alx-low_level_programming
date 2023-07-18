3c611b817f7f12f6261b1ef552cf19c2ca947b22
wonde21 <wendim878@gmail.com>
2023-07-18 08:34:12 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower - "Identify the lower than c"
+ * Return: Always 0
+ */
+
+int _islower(int c){
+	if (c < 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
