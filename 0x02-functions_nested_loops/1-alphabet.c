833c3a5bcfb0a3a2e66329faa09c488c23feeecf
wonde21 <wendim878@gmail.com>
2023-07-18 07:47:43 -0700

    1-alphabet.c added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print lower alphabet
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+}
