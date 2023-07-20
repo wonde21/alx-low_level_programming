eb48e0ff34d7fc3fc0f42b4e9f60acb878e66f6d
wonde21 <wendim878@gmail.com>
2023-07-20 05:10:11 -0700

    0-isupper.c fixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 #include <ctype.h>
 
 /**
- * int _isupper - cheack the apper or lower caseness of char
- * @n: a char to be cheack
- * Return: 1 if upper 0 if lowwer
+ * _isupper - cheack the apper or lower caseness of char
+ * @c: int type a char to be cheack
+ * Return: 1 if upper 0 if lower
  */
 
 int _isupper(int c)
