dd47e9d791985e69a0d657faa07fac60ee3ff232
wonde21 <wendim878@gmail.com>
2023-07-20 05:58:43 -0700

    3-print_numbers.c fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - print from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
@@ -12,6 +13,6 @@ void print_numbers(void)
 	{
 		_putchar(i);
 	}
-	
+
 	_putchar('\n');
 }
