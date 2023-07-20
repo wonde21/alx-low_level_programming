1641c2206a2da5ec0c50079b6e2745e43ac3d992
wonde21 <wendim878@gmail.com>
2023-07-20 06:08:24 -0700

    4-print_most_numbers.c fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * print_most_numbers - print from 0 to 9 except 2 and 4
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int n = 0;
 
-	if ((n != 2) && (n != 4))
+	if ((n != '2') && (n != '4'))
 	{
 		_putchar(n);
 	}
