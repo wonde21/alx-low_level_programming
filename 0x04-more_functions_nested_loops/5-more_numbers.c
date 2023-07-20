cd7052092a9664cfc97844c7572a8426ab334ce7
wonde21 <wendim878@gmail.com>
2023-07-20 06:24:48 -0700

    5-more_numbers.c added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for ( i = '0'; i <= '10'; i++)
+	{
+		j = '0';
+
+		while ( j <= '14')
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
+	}
+
+}
