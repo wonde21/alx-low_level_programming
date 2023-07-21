abf14bed07a68cb60eecdd8c0557275b34ce47d6
wonde21 <wendim878@gmail.com>
2023-07-21 03:51:33 -0700

    10-print_triangle.c added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print tringle with #
+ * @size: size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{       
+	int i;
+	
+	for (i = 1; i <= size; i++)
+	{
+		int n = 1;
+		
+		while(n <= size - i)
+		{
+			_putchar(' ');
+		}
+
+		int j = 1;
+			
+		while(j <= i)
+		{
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
